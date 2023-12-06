bd285b2d148d25880596faf8360c32b5c08f7090
Kim na young <kimna041208@gmail.com>
2023-12-07 05:20:51 +0900

    Design: 마이페이지 편집 UI

--- app/src/main/java/com/example/tripwith/MainActivity.kt
+++ app/src/main/java/com/example/tripwith/MainActivity.kt
@@ -13,19 +13,13 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
-        bottomNavigationView.itemIconTintList = null
+        val intent = Intent(this, MyPageEdit::class.java)
+        startActivity(intent)
 
-        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
-        navController?.let {
-            bottomNavigationView.setupWithNavController(it)
-        }
 
     }
 
